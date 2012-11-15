743be4d8920aca64bf9c1725f652fc6a43e7b92c
Lautaro Petaccio <lausuper@gmail.com>
2012-11-15 15:05:37 -0300

    Implementado iterador

--- ArbolCategorias.cpp
+++ ArbolCategorias.cpp
@@ -1,8 +1,10 @@
 #include "ArbolCategorias.h"
+#include <iostream>
+using namespace std;
 
 ArbolCategorias::ArbolCategorias(const Categoria& c) : categorias() {
-	estr_cat *nuevaCategoria = new estr_cat(1, c);
-	categorias.Definir(c, (*nuevaCategoria));
+	estr_cat nuevaCategoria = estr_cat(1, c);
+	categorias.Definir(c, nuevaCategoria);
 }
 
 ArbolCategorias::~ArbolCategorias() {
@@ -10,10 +12,11 @@ ArbolCategorias::~ArbolCategorias() {
 }
 
 void ArbolCategorias::agregarCategoria(const Categoria& c, const Categoria& h) {
-	estr_cat catPadre = categorias.Obtener(c);
+	estr_cat &catPadre = categorias.Obtener(c);
 	estr_cat *nuevaCategoria = new estr_cat(categorias.CantidadClaves()+1, h);
 	categorias.Definir(h, *(nuevaCategoria));
 	catPadre.hijos.AgregarRapido(nuevaCategoria);
+	estr_cat catPadre2 = categorias.Obtener(c);
 }
 
 Categoria& ArbolCategorias::raiz() {
@@ -25,22 +28,23 @@ int ArbolCategorias::id(const Categoria& c) {
 }
 
 ArbolCategorias::IteradorCategoriasHijas ArbolCategorias::categoriasHijas(const Categoria& c) {
-	return ArbolCategorias::IteradorCategoriasHijas();
+	return IteradorCategoriasHijas(categorias.Obtener(c).hijos); //Se puede devolver asi?
 }
 
-ArbolCategorias::IteradorCategoriasHijas::IteradorCategoriasHijas() {
-
+ArbolCategorias::IteradorCategoriasHijas::IteradorCategoriasHijas(Conj<estr_cat*> &conjHijos) {
+	this->it = conjHijos.CrearIt();
 }
 
 bool ArbolCategorias::IteradorCategoriasHijas::HaySiguiente() {
-	return false;
+	return it.HaySiguiente();
 }
 
 const Categoria& ArbolCategorias::IteradorCategoriasHijas::Siguiente() {
-	return "";
+	return it.Siguiente()->nombre;
 }
 
 void ArbolCategorias::IteradorCategoriasHijas::Avanzar() {
+	it.Avanzar();
 }
 
 ArbolCategorias::IteradorCategoriasHijas::~IteradorCategoriasHijas() {
--- ArbolCategorias.h
+++ ArbolCategorias.h
@@ -6,29 +6,7 @@
 #include "DiccTrie.h"
 
 class ArbolCategorias {
-public:
-
-	class IteradorCategoriasHijas {
-		IteradorCategoriasHijas();
-		friend class ArbolCategorias;
-
-	public:
-		bool HaySiguiente();
-		const Categoria& Siguiente();
-		void Avanzar();
-		virtual ~IteradorCategoriasHijas();
-	};
-
-	ArbolCategorias(); // No se deberia crear sin raiz
-	ArbolCategorias(const Categoria&);
-	void agregarCategoria(const Categoria& c, const Categoria& h);
-	Categoria& raiz();
-	int id(const Categoria&);
-	IteradorCategoriasHijas categoriasHijas(const Categoria& c);
-	virtual ~ArbolCategorias();
-
 private:
-
 	class estr_cat {
 	public:
 		Nat id;
@@ -42,6 +20,29 @@ private:
 
 	estr_cat root;
 	DiccTrie<estr_cat> categorias;
+
+public:
+	class IteradorCategoriasHijas {
+		IteradorCategoriasHijas(Conj<estr_cat*> &conjHijos);
+		friend class ArbolCategorias;
+
+	public:
+		bool HaySiguiente();
+		const Categoria& Siguiente();
+		void Avanzar();
+		virtual ~IteradorCategoriasHijas();
+	private:
+		Conj<estr_cat*>::Iterador it;
+	};
+
+	ArbolCategorias(); // No se deberia crear sin raiz
+	ArbolCategorias(const Categoria&);
+	void agregarCategoria(const Categoria& c, const Categoria& h);
+	Categoria& raiz();
+	int id(const Categoria&);
+	IteradorCategoriasHijas categoriasHijas(const Categoria& c);
+	virtual ~ArbolCategorias();
+
 };
 
 #endif /* ARBOLCATEGORIAS_H_ */

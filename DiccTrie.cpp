7ca46fb489f1786f3b8f4a66e48f5e0a4b21de7e
Leandro Lovisolo <leandro@leandro.me>
2012-11-17 13:15:11 -0300

    DiccTrie: se usa string en vez de Categoria y se usa una struct en vez de clase para representar nodos

--- DiccTrie.cpp
+++ DiccTrie.cpp
@@ -4,7 +4,7 @@ DiccTrie<T>::DiccTrie() : raiz() {
 }
 
 template<class T>
-void DiccTrie<T>::Definir(const Categoria &c, const T& s) {
+void DiccTrie<T>::Definir(const string &c, const T& s) {
 	estr_nodo *actual = &this->raiz;
 	for(Nat i=0;i<c.size();i++) {
 		if(actual->hijos[Nat(c[i])] == 0){
@@ -19,7 +19,7 @@ void DiccTrie<T>::Definir(const Categoria &c, const T& s) {
 }
 
 template<class T>
-T& DiccTrie<T>::Obtener(const Categoria &c) {
+T& DiccTrie<T>::Obtener(const string &c) {
 	estr_nodo *actual = &this->raiz;
 	for(Nat i=0;i<c.size();i++) {
 		actual = actual->hijos[c[i]];
@@ -31,4 +31,23 @@ T& DiccTrie<T>::Obtener(const Categoria &c) {
 template<class T>
 const Nat DiccTrie<T>::CantidadDeClaves() {
 	return this->cantidadDeClaves;
+}
+
+template<class T>
+DiccTrie<T>::estr_nodo::estr_nodo() : significado(0), hijos(256) {
+	for(Nat i=0;i<hijos.Tamanho();i++) {
+		hijos.Definir(i, 0);
+	}
+}
+
+template<class T>
+DiccTrie<T>::estr_nodo::~estr_nodo() {
+	if(significado != 0) {
+		delete significado;
+	}
+	for(Nat i=0;i<hijos.Tamanho();i++) {
+		if(hijos[i] != 0) {
+			delete hijos[i];
+		}
+	}
 }
--- DiccTrie.h
+++ DiccTrie.h
@@ -1,39 +1,28 @@
-#ifndef DICCTRIE_INCLUDED
-#define DICCTRIE_INCLUDED
+#ifndef DICCTRIE_H_
+#define DICCTRIE_H_
 
 #include "modulos/aed2.h"
 
-template<class T> class DiccTrie{
-private:
-	class estr_nodo{
-	public:
+template<class T>
+class DiccTrie {
+	struct estr_nodo {
 		T *significado;
 		Arreglo<estr_nodo*> hijos;
-		estr_nodo() : significado(0), hijos(256) {
-			for(Nat i=0;i<hijos.Tamanho();i++) {
-				hijos.Definir(i, 0);
-			}
-		}
-		~estr_nodo() {
-			if(significado != 0) {
-				delete significado;
-			}
-			for(Nat i=0;i<hijos.Tamanho();i++) {
-				if(hijos[i] != 0) {
-					delete hijos[i];
-				}
-			}
-		}
+
+		estr_nodo();
+		~estr_nodo();
 	};
+
 	Nat cantidadDeClaves;
 	estr_nodo raiz;
+
 public:
 	DiccTrie();
-	void Definir(const Categoria &c, const T& s);
-	T& Obtener(const Categoria &c);
+	void Definir(const string &c, const T& s);
+	T& Obtener(const string &c);
 	const Nat CantidadDeClaves();
 };
 
 #include "DiccTrie.cpp"
 
-#endif
+#endif /* DICCTRIE_H_ */

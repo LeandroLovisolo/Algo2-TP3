e0488d137f3c6969da6b5770fb856845fd92e6fb
Leandro Lovisolo <leandro@leandro.me>
2012-11-29 01:48:33 -0300

    Suite de tests del docente.

--- tp3.cpp
+++ tp3.cpp
@@ -2,49 +2,93 @@
 #include "Driver.h"
 #include "aed2_tests.h"
 
-void trieDefineYObtieneCorrectamente() {
-	DiccTrie<String> d;
-	/**
-	 * raíz
-	 *  |- f
-	 *  |  \- o
-	 *  |     \- o => bar
-	 *  \- c
-	 *     \- a
-	 *        |- s
-	 *        |  \- a => house
-	 *        \- c
-	 *           \- a => crap
-	 */
-
-	ASSERT_EQ(d.CantidadDeClaves(), 0);
-
-	d.Definir("foo", "bar");
-	ASSERT_EQ(d.CantidadDeClaves(), 1);
-
-	d.Definir("casa", "house");
-	ASSERT_EQ(d.CantidadDeClaves(), 2);
-
-	d.Definir("caca", "crap");
-	ASSERT_EQ(d.CantidadDeClaves(), 3);
-
-	ASSERT_EQ(d.Obtener("foo"), "bar");
-	ASSERT_EQ(d.Obtener("casa"), "house");
-	ASSERT_EQ(d.Obtener("caca"), "crap");
+// funcion auxiliar para mostrar accesos ordenados de la categoria
+void mostrarLinksOrdenados(Driver &d, const Categoria& cat)
+{
+    cout << "Links Ordenados: " << cat << endl;
+    int n = d.cantLinks(cat);
+    for(int i = 0; i < n; i++) {
+        cout << i << "\t"
+             << d.obtenerCategoriaIesimoLinkOrdenadoPorAccesos(cat, i) << "\t"
+             << d.obtenerIesimoLinkOrdenadoPorAccesos(cat, i) << "\t"
+             << d.obtenerCantidadAccesosIesimoLinkOrdenadoPorAccesos(cat, i) << endl;
+    }
 }
 
-void driverCtor() {
-	Driver d;
-}
-
-void driverPuedeAgregarCategorias() {
-	Driver d;
+void armarArbol(Driver& d) {
     /**
-     * cat1
-     *  |- cat2
-     *  \- cat3
-     *      \- cat4
-     */
+* cat1
+* |- cat2
+* | |- cat4
+* | \- cat5
+* |
+* \- cat3
+* \- cat6
+*/
+    d.nuevoArbol("cat1");
+    d.agregarCategoria("cat1","cat2");
+    d.agregarCategoria("cat1","cat3");
+    d.agregarCategoria("cat2","cat4");
+    d.agregarCategoria("cat2","cat5");
+    d.agregarCategoria("cat3","cat6");
+}
+
+void testDriverCtor() {
+Driver d;
+}
+
+void testArbolRaiz() {
+Driver d;
+    d.nuevoArbol("cat1");
+    ASSERT_EQ(d.cantCategoriasHijas("cat1"), 0);
+    ASSERT_EQ(d.raiz(), "cat1");
+}
+
+void testArbolConHijos() {
+Driver d;
+    d.nuevoArbol("cat1");
+    d.agregarCategoria("cat1","cat2");
+    ASSERT_EQ(d.raiz(), "cat1");
+    ASSERT_EQ(d.cantCategoriasHijas("cat1"), 1);
+    ASSERT_EQ(d.obtenerIesimaCategoriaHija("cat1", 0), "cat2");
+}
+
+void testArbolConHijosQueRequiereCopia__OPCIONAL() {
+Driver d;
+    String *c1 = new String("cat1");
+    String *c1p = new String("cat1");
+    String *c2 = new String("cat2");
+    d.nuevoArbol(*c1);
+    d.agregarCategoria(*c1p, *c2);
+    delete c1;
+    delete c1p;
+    delete c2;
+    ASSERT_EQ(d.raiz(), "cat1");
+    ASSERT_EQ(d.cantCategoriasHijas("cat1"), 1);
+    ASSERT_EQ(d.obtenerIesimaCategoriaHija("cat1", 0), "cat2");
+}
+
+void testArbolIDsConsecutivos()
+{
+    Driver d;
+    armarArbol(d);
+
+    ASSERT_EQ(d.id("cat1"),1);
+    ASSERT_EQ(d.id("cat2"),2);
+    ASSERT_EQ(d.id("cat3"),3);
+    ASSERT_EQ(d.id("cat4"),4);
+    ASSERT_EQ(d.id("cat5"),5);
+    ASSERT_EQ(d.id("cat6"),6);
+}
+
+void testArbolConVariasCategorias() {
+Driver d;
+    /**
+* cat1
+* |- cat2
+* \- cat3
+* \- cat4
+*/
     d.nuevoArbol("cat1");
     d.agregarCategoria("cat1","cat2");
     d.agregarCategoria("cat1","cat3");
@@ -55,67 +99,241 @@ void driverPuedeAgregarCategorias() {
     ASSERT_EQ(d.cantCategoriasHijas("cat3"), 1);
 }
 
-void driverPuedeAgregarLinks() {
-	Driver d;
-	/**
-	 * cat1         (link1)
-	 *  |- cat2     (link2, link3)
-	 *  \- cat3     ()
-	 *      |- cat4 (link4)
-	 *      \- cat5 ()
-	 */
-	d.nuevoArbol("cat1");
-	d.agregarCategoria("cat1","cat2");
-	d.agregarCategoria("cat1","cat3");
-	d.agregarCategoria("cat3","cat4");
-	d.agregarCategoria("cat3","cat5");
+void testArbolIteradorHijasConVariosElementos()
+{
+    Driver d;
+    armarArbol(d);
 
-	d.nuevoLink("link1", "cat1");
-	d.nuevoLink("link2", "cat2");
-	d.nuevoLink("link3", "cat2");
-	d.nuevoLink("link4", "cat4");
+    ASSERT(d.obtenerIesimaCategoriaHija("cat1",0) == "cat2" ||
+           d.obtenerIesimaCategoriaHija("cat1",0) == "cat3");
+    ASSERT(d.obtenerIesimaCategoriaHija("cat1",1) == "cat2" ||
+           d.obtenerIesimaCategoriaHija("cat1",1) == "cat3");
+    ASSERT(d.obtenerIesimaCategoriaHija("cat1",0) != d.obtenerIesimaCategoriaHija("cat1",1));
 
-	ASSERT_EQ(d.cantLinks("cat1"), 4);
-	ASSERT_EQ(d.cantLinks("cat2"), 2);
-	ASSERT_EQ(d.cantLinks("cat3"), 1);
-	ASSERT_EQ(d.cantLinks("cat4"), 1);
-	ASSERT_EQ(d.cantLinks("cat5"), 0);
 }
 
-void driverPuedeAccederLinks() {
-	Driver d;
-	d.nuevoArbol("raiz");
-	d.nuevoLink("link", "raiz");
-	for(int i = 1; i < 16; i = i * 2)
-		for(int j = 1; j <= i; j++)
-			d.acceso("link", i);
+void agregarLinks(Driver& d) {
+    /** categoria {links propios sin hijos}
+*
+* cat1 {link_A, link_B}
+* |- cat2 {link_C}
+* | |- cat4 {link_D}
+* | \- cat5
+* |
+* \- cat3
+* \- cat6 {link_E}
+*/
+    d.nuevoLink("link_A", "cat1");
+    d.nuevoLink("link_B", "cat1");
+    d.nuevoLink("link_C", "cat2");
+    d.nuevoLink("link_D", "cat4");
+    d.nuevoLink("link_E", "cat6");
 }
 
-void driverPuedeIterarLinks() {
-	Driver d;
-	d.nuevoArbol("raiz");
-	d.nuevoLink("link1", "raiz");
-	d.nuevoLink("link2", "raiz");
-	d.nuevoLink("link3", "raiz");
-	d.acceso("link1", 1);
-	d.acceso("link1", 1);
-	d.acceso("link2", 1);
-	d.acceso("link2", 1);
-	d.acceso("link2", 1);
-	d.acceso("link3", 1);
-
-	ASSERT_EQ(d.obtenerIesimoLinkOrdenadoPorAccesos("raiz", 0), "link2");
-	ASSERT_EQ(d.obtenerIesimoLinkOrdenadoPorAccesos("raiz", 1), "link1");
-	ASSERT_EQ(d.obtenerIesimoLinkOrdenadoPorAccesos("raiz", 2), "link3");
+void accederLinks(Driver& d) {
+    /** categoria {links propios} : [links ordenados por acceso]
+*
+* cat1 {link_A, link_B} : [link_A,link_C,link_B] ++ permutacion( [link_D, link_E] )
+* |- cat2 {link_C} : [link_C, link_D]
+* | |- cat4 {link_D} : [link_D]
+* | \- cat5 {} : []
+* |
+* \- cat3 {} : [link_E]
+* \- cat6 {link_E} : [link_E]
+*/
+    d.acceso("link_A",1);
+    d.acceso("link_A",1);
+    d.acceso("link_A",1);
+    d.acceso("link_A",2);
+    d.acceso("link_B",2);
+    d.acceso("link_B",2);
+    d.acceso("link_C",3);
+    d.acceso("link_C",3);
+    d.acceso("link_C",3);
 }
 
+void testSistemaCuentaLinksEnCategoriasDescendientes() {
+Driver d;
+    armarArbol(d);
+    agregarLinks(d);
+
+    ASSERT_EQ(d.cantLinks("cat1"), 5);
+    ASSERT_EQ(d.cantLinks("cat2"), 2);
+    ASSERT_EQ(d.cantLinks("cat3"), 1);
+}
+
+void testSistemaLosLinksEstanOrdenadosSegunEspecificacion() {
+Driver d;
+    armarArbol(d);
+    agregarLinks(d);
+    accederLinks(d);
+
+    ASSERT_EQ(d.obtenerIesimoLinkOrdenadoPorAccesos("cat1",0), "link_A");
+    ASSERT_EQ(d.obtenerCategoriaIesimoLinkOrdenadoPorAccesos("cat1",0), "cat1");
+    ASSERT_EQ(d.obtenerCantidadAccesosIesimoLinkOrdenadoPorAccesos("cat1",0), 4);
+
+    ASSERT_EQ(d.obtenerIesimoLinkOrdenadoPorAccesos("cat1",1), "link_C");
+    ASSERT_EQ(d.obtenerCategoriaIesimoLinkOrdenadoPorAccesos("cat1",1), "cat2");
+    ASSERT_EQ(d.obtenerCantidadAccesosIesimoLinkOrdenadoPorAccesos("cat1",1), 3);
+
+    ASSERT_EQ(d.obtenerIesimoLinkOrdenadoPorAccesos("cat1",2), "link_B");
+    ASSERT_EQ(d.obtenerCategoriaIesimoLinkOrdenadoPorAccesos("cat1",2), "cat1");
+    ASSERT_EQ(d.obtenerCantidadAccesosIesimoLinkOrdenadoPorAccesos("cat1",2), 2);
+
+    ASSERT_EQ(d.obtenerIesimoLinkOrdenadoPorAccesos("cat2",0), "link_C");
+    ASSERT_EQ(d.obtenerCategoriaIesimoLinkOrdenadoPorAccesos("cat2",0), "cat2");
+    ASSERT_EQ(d.obtenerCantidadAccesosIesimoLinkOrdenadoPorAccesos("cat2",0), 3);
+}
+
+int indiceLinkOrdenado(Driver &d, const Categoria& cat, const Link& l)
+{
+    int n = d.cantLinks(cat);
+    for(int i = 0; i < n; i++) {
+        if (d.obtenerIesimoLinkOrdenadoPorAccesos(cat, i) == l)
+            return i;
+    }
+    ASSERT(false);
+    return -1;
+}
+
+void testSistemaLosLinksPierdenAccesosSegunVentana() {
+Driver d;
+    armarArbol(d);
+    agregarLinks(d);
+    accederLinks(d);
+
+    // Corrimiento de ventana
+    d.acceso("link_C",4);
+    ASSERT_EQ(d.obtenerIesimoLinkOrdenadoPorAccesos("cat1", 0), "link_C");
+    ASSERT_EQ(d.obtenerCategoriaIesimoLinkOrdenadoPorAccesos("cat1",0), "cat2");
+    ASSERT_EQ(d.obtenerCantidadAccesosIesimoLinkOrdenadoPorAccesos("cat1",0), 4);
+
+    int pos_link_A = indiceLinkOrdenado(d, "cat1", "link_A");
+    // por la ventana link A queda con 1 acceso y eso lo lleva al final
+    ASSERT_EQ(d.obtenerCantidadAccesosIesimoLinkOrdenadoPorAccesos("cat1", pos_link_A), 1);
+    ASSERT(pos_link_A == 2);
+
+    // Corrimiento de ventana
+    d.acceso("link_A",4);
+    d.acceso("link_A",4);
+    d.acceso("link_A",4);
+    d.acceso("link_A",4);
+    ASSERT_EQ(d.obtenerIesimoLinkOrdenadoPorAccesos("cat1",0), "link_A");
+    ASSERT_EQ(d.obtenerCategoriaIesimoLinkOrdenadoPorAccesos("cat1",0), "cat1");
+    ASSERT_EQ(d.obtenerCantidadAccesosIesimoLinkOrdenadoPorAccesos("cat1",0), 5);
+    // La cat2 no cambia
+    ASSERT_EQ(d.obtenerIesimoLinkOrdenadoPorAccesos("cat2",0), "link_C");
+    ASSERT_EQ(d.obtenerCategoriaIesimoLinkOrdenadoPorAccesos("cat2",0), "cat2");
+    ASSERT_EQ(d.obtenerCantidadAccesosIesimoLinkOrdenadoPorAccesos("cat2",0), 4);
+}
+
+void testSistemaLinksPuedenTenerDistintosAccesosYCambiarOrden()
+{
+    Driver d;
+    armarArbol(d);
+
+    /** categoria {links propios sin hijos}
+*
+* cat1 {link_C}
+* |- cat2 {link_A, link_B}
+*/
+    d.nuevoLink("link_A", "cat2");
+    d.nuevoLink("link_B", "cat2");
+    d.nuevoLink("link_C", "cat1");
+
+//TODO A 100 0 0 0
+// B 2
+// C 0 1
+    for(int i=0;i<100;i++) {
+        d.acceso("link_A", 1);
+    }
+    d.acceso("link_B", 2);
+    d.acceso("link_B", 2);
+d.acceso("link_C", 4);
+
+    ASSERT_EQ(d.cantLinks("cat1"), 3);
+    ASSERT_EQ(d.cantLinks("cat2"), 2);
+
+ASSERT_EQ(d.obtenerIesimoLinkOrdenadoPorAccesos("cat2",0), "link_A");
+    ASSERT_EQ(d.obtenerCategoriaIesimoLinkOrdenadoPorAccesos("cat2",0), "cat2");
+    ASSERT_EQ(d.obtenerCantidadAccesosIesimoLinkOrdenadoPorAccesos("cat2",0), 100);
+
+ASSERT_EQ(d.obtenerIesimoLinkOrdenadoPorAccesos("cat2",1), "link_B");
+    ASSERT_EQ(d.obtenerCategoriaIesimoLinkOrdenadoPorAccesos("cat2",1), "cat2");
+    ASSERT_EQ(d.obtenerCantidadAccesosIesimoLinkOrdenadoPorAccesos("cat2",1), 2);
+
+
+    ASSERT_EQ(d.obtenerIesimoLinkOrdenadoPorAccesos("cat1",0), "link_B");
+    ASSERT_EQ(d.obtenerCategoriaIesimoLinkOrdenadoPorAccesos("cat1",0), "cat2");
+    ASSERT_EQ(d.obtenerCantidadAccesosIesimoLinkOrdenadoPorAccesos("cat1",0), 2);
+
+    ASSERT_EQ(d.obtenerIesimoLinkOrdenadoPorAccesos("cat1",1), "link_C");
+    ASSERT_EQ(d.obtenerCategoriaIesimoLinkOrdenadoPorAccesos("cat1",1), "cat1");
+    ASSERT_EQ(d.obtenerCantidadAccesosIesimoLinkOrdenadoPorAccesos("cat1",1), 1);
+
+    ASSERT_EQ(d.obtenerIesimoLinkOrdenadoPorAccesos("cat1",2), "link_A");
+    ASSERT_EQ(d.obtenerCategoriaIesimoLinkOrdenadoPorAccesos("cat1",2), "cat2");
+    ASSERT_EQ(d.obtenerCantidadAccesosIesimoLinkOrdenadoPorAccesos("cat1",2), 0);
+}
+
+void testSistemaAndaPorCopia__OPCIONAL() {
+    Driver d;
+    String *c1 = new String("cat1");
+    String *c1p = new String("cat1");
+    String *c2 = new String("cat2");
+    d.nuevoArbol(*c1);
+    d.agregarCategoria(*c1p, *c2);
+
+    delete c1;
+    delete c1p;
+    delete c2;
+
+    String *lA = new String("link_A");
+    String *lAp = new String("link_A");
+    String *c2p = new String("cat2");
+    d.nuevoLink(*lA, *c2p);
+    delete lA;
+    delete c2p;
+
+    d.acceso(*lAp, 1);
+    delete lAp;
+
+    ASSERT_EQ(d.obtenerIesimoLinkOrdenadoPorAccesos("cat1",0), "link_A");
+    ASSERT_EQ(d.obtenerCategoriaIesimoLinkOrdenadoPorAccesos("cat1",0), "cat2");
+    ASSERT_EQ(d.obtenerCantidadAccesosIesimoLinkOrdenadoPorAccesos("cat1",0), 1);
+}
+
+void testSistemaCategoriasSinLinkTieneIterador()
+{
+    Driver d;
+    armarArbol(d);
+
+    d.nuevoLink("link_A", "cat4");
+
+    ASSERT_EQ(d.cantLinks("cat1"), 1);
+    ASSERT_EQ(d.cantLinks("cat2"), 1);
+    ASSERT_EQ(d.cantLinks("cat3"), 0);
+    ASSERT_EQ(d.cantLinks("cat4"), 1);
+    ASSERT_EQ(d.cantLinks("cat5"), 0);
+    ASSERT_EQ(d.cantLinks("cat6"), 0);
+}
+
+//
 int main(void) {
-	RUN_TEST(trieDefineYObtieneCorrectamente);
-    RUN_TEST(driverCtor);
-    RUN_TEST(driverPuedeAgregarCategorias);
-    RUN_TEST(driverPuedeAgregarLinks);
-    RUN_TEST(driverPuedeAccederLinks);
-    RUN_TEST(driverPuedeIterarLinks);
+    RUN_TEST(testDriverCtor);
+    RUN_TEST(testArbolRaiz);
+    RUN_TEST(testArbolConHijos);
+    RUN_TEST(testArbolIDsConsecutivos);
+    RUN_TEST(testArbolConVariasCategorias);
+    RUN_TEST(testArbolIteradorHijasConVariosElementos);
+    RUN_TEST(testSistemaCuentaLinksEnCategoriasDescendientes);
+    RUN_TEST(testSistemaLosLinksEstanOrdenadosSegunEspecificacion);
+    RUN_TEST(testSistemaLosLinksPierdenAccesosSegunVentana);
+    RUN_TEST(testSistemaLinksPuedenTenerDistintosAccesosYCambiarOrden);
+    RUN_TEST(testSistemaCategoriasSinLinkTieneIterador);
 
-	return 0;
+    RUN_TEST(testArbolConHijosQueRequiereCopia__OPCIONAL);
+    RUN_TEST(testSistemaAndaPorCopia__OPCIONAL);
+
+    return 0;
 }
